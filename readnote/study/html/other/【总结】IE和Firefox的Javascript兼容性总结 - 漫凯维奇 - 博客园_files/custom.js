c5f918156c55227a676868fb4579b0df6d1d1c19
yhyuan <yhyuan@shendu.com>
2016-06-17 18:10:25 +0800

    c

---
+++ readnote/study/html/other/【总结】IE和Firefox的Javascript兼容性总结 - 漫凯维奇 - 博客园_files/custom.js
@@ -0,0 +1,11 @@
+ function wiky_nav(){
+    $('.wiky_nav').kwicks({
+		   duration: 500,   
+        max: 170,  
+        spacing:  0  
+    });
+}  
+
+ $(document).ready(function(){					
+	wiky_nav();
+});
---
+++ readnote/study/html/ssh2/Guava学习笔记：Google Guava 类库简介 - peida - 博客园_files/1910329.js
@@ -0,0 +1,10 @@
+document.write ('<a href="http://www.51.la/?1910329" target="_blank" title="51.La &#x7F51;&#x7AD9;&#x6D41;&#x91CF;&#x7EDF;&#x8BA1;&#x7CFB;&#x7EDF;"><img alt="51.La &#x7F51;&#x7AD9;&#x6D41;&#x91CF;&#x7EDF;&#x8BA1;&#x7CFB;&#x7EDF;" src="http://icon.51.la/icon_0.gif" style="border:none" /></a>\n');
+var a0329tf="51la";var a0329pu="";var a0329pf="51la";var a0329su=window.location;var a0329sf=document.referrer;var a0329of="";var a0329op="";var a0329ops=1;var a0329ot=1;var a0329d=new Date();var a0329color="";if (navigator.appName=="Netscape"){a0329color=screen.pixelDepth;} else {a0329color=screen.colorDepth;}
+try{a0329tf=top.document.referrer;}catch(e){}
+try{a0329pu =window.parent.location;}catch(e){}
+try{a0329pf=window.parent.document.referrer;}catch(e){}
+try{a0329ops=document.cookie.match(new RegExp("(^| )AJSTAT_ok_pages=([^;]*)(;|$)"));a0329ops=(a0329ops==null)?1: (parseInt(unescape((a0329ops)[2]))+1);var a0329oe =new Date();a0329oe.setTime(a0329oe.getTime()+60*60*1000);document.cookie="AJSTAT_ok_pages="+a0329ops+ ";path=/;expires="+a0329oe.toGMTString();a0329ot=document.cookie.match(new RegExp("(^| )AJSTAT_ok_times=([^;]*)(;|$)"));if(a0329ot==null){a0329ot=1;}else{a0329ot=parseInt(unescape((a0329ot)[2])); a0329ot=(a0329ops==1)?(a0329ot+1):(a0329ot);}a0329oe.setTime(a0329oe.getTime()+365*24*60*60*1000);document.cookie="AJSTAT_ok_times="+a0329ot+";path=/;expires="+a0329oe.toGMTString();}catch(e){}
+try{if(document.cookie==""){a0329ops=-1;a0329ot=-1;}}catch(e){}
+a0329of=a0329sf;if(a0329pf!=="51la"){a0329of=a0329pf;}if(a0329tf!=="51la"){a0329of=a0329tf;}a0329op=a0329pu;try{lainframe}catch(e){a0329op=a0329su;}
+a0329src='http://web.51.la:82/go.asp?svid=8&id=1910329&tpages='+a0329ops+'&ttimes='+a0329ot+'&tzone='+(0-a0329d.getTimezoneOffset()/60)+'&tcolor='+a0329color+'&sSize='+screen.width+','+screen.height+'&referrer='+escape(a0329of)+'&vpage='+escape(a0329op)+'&vvtime='+a0329d.getTime();
+setTimeout('a0329img = new Image;a0329img.src=a0329src;',0);
