0f1e15ad2d50f5373eaac6792ac92a09e667c566
wenchang.shi <1446008778@qq.com>
2016-06-19 15:17:42 +0800

    c

---
+++ master/src/main/java/com/jhmadia/master/Start.java
@@ -0,0 +1,24 @@
+/*========================================
+ * 项目名：婧涵影视-master
+ * 文件名：Start.java
+ * 备    注：中国广东省广州市番禺区大石镇植村村五路(海珠区客村新港中路影盛花园C栋2508室)
+ * CopyRigth (C) 2015-2016 jhmedia 婧涵影视
+ *========================================*/
+package com.jhmadia.master;
+
+/**
+ * 类   名：启动类
+ * <pre>
+ * 概   要：使用jetty这个servlet容器启动本项目
+ * 作成者：Seki2016
+ * 作成日：2016年6月19日
+ *
+ * </pre>
+ */
+public class Start {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
