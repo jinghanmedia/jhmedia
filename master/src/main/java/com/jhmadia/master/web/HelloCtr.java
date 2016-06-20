ab36b902a8cd9554fa1b2e1578e037cab6c32f24
yhyuan <yhyuan@shendu.com>
2016-06-20 18:30:07 +0800

    c

--- master/src/main/java/com/jhmadia/master/web/HelloCtr.java
+++ master/src/main/java/com/jhmadia/master/web/HelloCtr.java
@@ -9,16 +9,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.jhmadia.master.util.PageData;
+import com.jhmadia.master.web.common.BaseController;
+
 @Controller
 @RequestMapping("/helloworld")
-public class HelloCtr {
+public class HelloCtr extends BaseController {
 
     @RequestMapping("/helloSay")
     public ModelAndView hellSay(HttpServletRequest request) {
         ModelAndView mv = new ModelAndView();
+        PageData pd = this.getPageData();
 
         String userName = "";
-        userName = request.getParameter("username");
+        userName = pd.getString("username");
 
         Map<String, String> user = new HashMap<String, String>();
         user.put("username", userName);
